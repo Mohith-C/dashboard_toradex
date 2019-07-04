7bfc6cc8d8317c2d19b258b0ae13ce77c4733021
mohith-c <mohith.c@claysol.com>
2019-07-04 16:42:29 +0530

    adding build script

---
+++ build.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+# Prepare files needed for flashing a Apalis/Colibri iMX6 module
+#
+# inspired by meta-fsl-arm/classes/image_types_fsl.bbclass
+
+# exit on error
+set -e
+
+
+
+Usage()
+{
+	echo ""
+	echo "Build for tarform "
+	echo ""
+}
+
+cd ./src/rootfs/buildroot-2019.02.3
+echo "**************configuring builldroot ***********************"
+make toradex_apalis_imx6_defconfig_tarform
+
+echo "**************Building builldroot ***********************"
+make
+
+cd ../../app/tarform
+echo "**************Building App ***********************"
+../../rootfs/buildroot-2019.02.2/output/host/bin/qmake
+
+make
